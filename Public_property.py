0698b44a3c7b4122d634c5184bf5ebd70620ecc6
AkhilChoraria <chorariaakhil03@gmail.com>
2022-06-06 17:23:51 +0530

    Create Public_property.py

---
+++ Public_property.py
@@ -0,0 +1,50 @@
+class Publicproperties:
+    """
+    These are properties such as Railways, Airport etc
+    according to Plato
+    """
+
+    def __init__(self):
+        self.owner = False
+        self.acquisition_cost = 0
+        self.rent = 0
+        self.sell_price = 0
+
+    def is_owner(self):
+        return self.owner
+
+
+class Harbour(Publicproperties):
+    def __init__(self):
+        super().__init__()
+        self.owner = False
+        self.acquisition_cost = 100
+        self.rent = 35
+        self.sell_price = 80
+
+
+class Railway(Publicproperties):
+    def __init__(self):
+        super().__init__()
+        self.owner = False
+        self.acquisition_cost = 150
+        self.rent = 35
+        self.sell_price = 120
+
+
+class Electricity(Publicproperties):
+    def __init__(self):
+        super().__init__()
+        self.owner = False
+        self.acquisition_cost = 200
+        self.rent = 35
+        self.sell_price = 160
+
+
+class Airport(Publicproperties):
+    def __init__(self):
+        super().__init__()
+        self.owner = False
+        self.acquisition_cost = 250
+        self.rent = 35
+        self.sell_price = 200
