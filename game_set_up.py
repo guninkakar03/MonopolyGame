bfba4e8198b10980c706880c18cfe4e96e244298
guninkakar03 <guninkakar03@gmail.com>
2022-06-09 03:53:10 +0530

    -

--- game_set_up.py
+++ game_set_up.py
@@ -22,31 +22,13 @@ def game_formation() -> None:
             player.reduce_jail_sentence()
         else:
             dice = randint(1, 6)
-            # seed(10)
-            # print(dice)
-
             current_tile = player.move_forward(dice).item
             if dice == 6:
                 new_dice = randint(1, 6)
                 current_tile = player.move_forward(new_dice).item
 
             if isinstance(current_tile, City):
-
-                if not current_tile.owner and current_tile not in player.properties:  # what if the owner is the person itself? # also check if there is an owner
-                    player.pay_rent(current_tile)
-                    player_receive = who_property(list_players, current_tile)
-                    player_receive.receive_rent(current_tile)
-                    # Idk why is this a problem -> since type contact is city
-                    # and this not city but its sub class
-
-                elif player.cash_in_hand >= current_tile.acquisition_cost:
-                    verdict = input(F"Do you want to buy the property for{current_tile.acquisition_cost}?(Y/N)")
-                    if verdict == 'Y':
-                        player.buy_property(current_tile)
-
-                    # ask for the options of what to do
-                    print("yp")
-                # else the player has insufficient fund -> no options by py qt
+                property_decision(current_tile, player, list_players)
 
             elif isinstance(current_tile, Jail):  # stuck for 3 rounds or pay certain amount to get out
                 verdict = input("stuck for 3 rounds or pay 150 to get out: ")  # py qt button
@@ -66,7 +48,7 @@ def game_formation() -> None:
         # if bankrupt, ie, if money is less than $-500
         if player.cash_in_hand < -500:
             # python qt line === you are bankrupt ===
-            list_players.delete_player(player)
+            list_players.delete_player(player) # I believe this would be a problem - let's discuss asap!
         if len(list_players) == 1:
             # python qt line === you won ===
             break
@@ -76,16 +58,23 @@ def game_formation() -> None:
         curr = curr.next  # gets the next player
 
 
-def jail_decision(player: Players, decision: str) -> None:
-    if decision == "pay":
-        player.cash_in_hand -= 150  # player pays $150 to get out, need to check whether wealth decreases too
+def property_decision(current_tile: City, player: Players, list_players: LinkedList) -> None:
+
+    # checks if the property is owned
+    if current_tile.owner and current_tile not in player.properties:
+
+        player.pay_rent(current_tile)
+        player_receive = who_property(list_players, current_tile)
+        player_receive.receive_rent(current_tile)
+
+
+    elif player.cash_in_hand >= current_tile.acquisition_cost:
+        verdict = input(F"Do you want to buy the property for{current_tile.acquisition_cost}?(Y/N)")
+        if verdict == 'Y':
+            player.buy_property(current_tile) # check this
     else:
-        player.send_to_jail()
-
-
-'''def buy_or_not(player: Players, decision: str) -> None:
-    if decision == 'Y':
-        player.buy_property()'''
+        pass
+        # py qt ===insufficient funds===
 
 
 def who_property(list_players: LinkedList, city: City) -> Players:
@@ -100,6 +89,21 @@ def who_property(list_players: LinkedList, city: City) -> Players:
         if city in player.properties:
             return player
         curr = curr.next
+
+
+def jail_decision(player: Players, decision: str) -> None:
+    if decision == "pay":
+        player.cash_in_hand -= 150  # player pays $150 to get out, need to check whether wealth decreases too
+    else:
+        player.send_to_jail()
+
+
+'''def buy_or_not(player: Players, decision: str) -> None:
+    if decision == 'Y':
+        player.buy_property()'''
+
+
+
 
 
 game_formation()
