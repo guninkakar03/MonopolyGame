daf98f83642716b74f1648dcb541357a5cca63bb
AkhilChoraria <chorariaakhil03@gmail.com>
2022-06-11 23:13:47 +0530

    Update Public_property.py

--- Public_property.py
+++ Public_property.py
@@ -7,8 +7,12 @@ class Publicproperties:
     def __init__(self):
         self.owner = False
         self.acquisition_cost = 0
-        self.rent = 0
+        self.rent = 35
         self.sell_price = 0
+        self.rent_with_two = 70
+        self.rent_with_three = 105
+        self.rent_with_four = 140
+        self.totalproperty=0
 
     def is_owner(self):
         return self.owner
@@ -17,34 +21,34 @@ class Publicproperties:
 class Harbour(Publicproperties):
     def __init__(self):
         super().__init__()
-        self.owner = False
+        # self.owner = False
         self.acquisition_cost = 100
-        self.rent = 35
+        # self.rent = 35
         self.sell_price = 80
 
 
 class Railway(Publicproperties):
     def __init__(self):
         super().__init__()
-        self.owner = False
+        # self.owner = False
         self.acquisition_cost = 150
-        self.rent = 35
+        # self.rent = 35
         self.sell_price = 120
 
 
 class Electricity(Publicproperties):
     def __init__(self):
         super().__init__()
-        self.owner = False
+        # self.owner = False
         self.acquisition_cost = 200
-        self.rent = 35
+        # self.rent = 35
         self.sell_price = 160
 
 
 class Airport(Publicproperties):
     def __init__(self):
         super().__init__()
-        self.owner = False
+        # self.owner = False
         self.acquisition_cost = 250
-        self.rent = 35
+        # self.rent = 35
         self.sell_price = 200
